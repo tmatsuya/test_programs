89196e2ca128992d21a22edaf06b54cbc3a13119
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2014-08-21 21:34:36 +0900

    bypass_ipstack: fixed

--- bypass_ipstack/count.c
+++ bypass_ipstack/count.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	int start,end;
-	scanf("%d", &start);
+	int start,end,i;
+	scanf("%d,%d,%d", &start,&i,&i);
 	sleep(10);
-	scanf("%d", &end);
+	scanf("%d,%d,%d", &end,&i,&i);
 	printf("start=%lu\n", start);
 	printf("end  =%lu\n", end);
 	printf("diff =%lu\n", end-start);
